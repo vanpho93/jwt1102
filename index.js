078256161e084d692c000dfb3f78535fb201798c
Admin <vanpho02@gmail.com>
2017-03-19 10:20:04 +0700

    heroku config

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
 
 app.get('/', (req, res) => res.send('Still alive'));
